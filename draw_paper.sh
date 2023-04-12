c81107b29ca6275b7be2034d4463e7ebc150359e
skyriaco <savvas.kyriacou@cern.ch>
2023-04-12 23:04:05 +0200

    instructions

--- draw_paper.sh
+++ draw_paper.sh
@@ -1,6 +1,6 @@
 python plotlimits_standalone.py  --scan fa3_fixed_19009.root  "Expected " kRed kSolid  --legendposition 0.2 0.6 0.6 0.9 --luminosity 77.4  --scanrange -1 1 --POI CMS_zz4l_fai1 --CMStext Preliminary --ytitle " -2 #Deltaln L" --xtitle "#Gamma (MeV)" --CLtextposition "right" --lettersize 0.2 scan_test_new --killpoints -2 -2
 
-cp scan_test_new.p* /eos/user/s/skyriaco/www/public_html/forLourdes
+#cp scan_test_new.p* /eos/user/s/skyriaco/www/public_html/forLourdes
 
 
 
